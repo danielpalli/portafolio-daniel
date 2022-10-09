700525a447aed4f144b2cbaa3b5257de82243070
DanielPalli <dpallicolque@frba.utn.edu.ar>
2022-10-09 12:09:37 -0300

    Update

--- src/app/modules/dashboard/pages/inicio/inicio.component.ts
+++ src/app/modules/dashboard/pages/inicio/inicio.component.ts
@@ -6,6 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.scss']
 })
 export class InicioComponent implements OnInit {
+  redes: any[] = [{
+    nombre: 'github',
+    img: '../../../../../assets/redes/github-logo.png',
+    url: 'https://github.com/danielpalli',
+  },
+  {
+    nombre: 'facebook',
+    img: '../../../../../assets/redes/facebook-logo.png',
+    url: 'https://www.facebook.com/fernando.palli.16',
+  },
+  {
+    nombre: 'twitter',
+    img: '../../../../../assets/redes/twitter-logo.png',
+    url: 'https://github.com/danielpalli',
+  },
+  {
+    nombre: 'instagram',
+    img: '../../../../../assets/redes/instagram-logo.webp',
+    url: 'https://github.com/danielpalli',
+  },
+
+
+
+]
 
   constructor() { }
 
