4ca347c369cc3c7f41162c89ff9f18a145d1e99c
Daniel Palli <dpallicolque@frba.utn.edu.ar>
2024-09-07 01:08:18 -0300

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  // apiUrl: 'https://portafolio-daniel-ten.vercel.app/assets/data/data.json',
-  apiUrl: 'http://localhost:4200/assets/data/data.json',
+  apiUrl: 'https://portafolio-daniel-ten.vercel.app/assets/data/data.json',
+  // apiUrl: 'http://localhost:4200/assets/data/data.json',
   production: false,
 };
